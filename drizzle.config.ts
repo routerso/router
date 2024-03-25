eb38e6cfc2c8093871c82b44608ab373d6818be0
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-03-25 15:42:21 -0600

    add shadcn

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "@/lib/db/config";
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+  schema: "lib/db/schema.ts",
+  out: "lib/db/drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.POSTGRES_URL || "",
+  },
+  verbose: true,
+  strict: true,
+});
