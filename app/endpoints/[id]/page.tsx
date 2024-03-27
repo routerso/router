6f48f6c198eba24cd70964b4f4b26223587d9208
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-03-27 14:35:09 -0600

    feat: add posting instructions page

--- app/endpoints/[id]/page.tsx
+++ app/endpoints/[id]/page.tsx
@@ -4,6 +4,8 @@ import { Breadcrumbs } from "@/components/parts/breadcrumbs";
 import { Header } from "@/components/parts/header";
 import { getEndpointById } from "@/lib/data/endpoints";
 import SchemaTable from "@/components/groups/endpoints/schema-table";
+import * as Craft from "@/components/craft/layout";
+import { Separator } from "@/components/ui/separator";
 
 const pageData = {
   title: "Endpoint",
@@ -17,13 +19,56 @@ export default async function Page({ params }: { params: { id: string } }) {
   const endpoint = await getEndpointById(params?.id);
   const schema = endpoint?.schema as GeneralSchema[];
 
+  const url = `https//router.so/api/${endpoint?.incrementId}`;
+  // TODO: generate API key when an endpoint is created
+  const apiKey = "8237lkasdf89789sadf";
+
+  //  ---------- TODO: make this into its own function ----------
+  const formattedSchema = new Object() as { [key: string]: ValidationType };
+  schema.forEach((field) => {
+    formattedSchema[field.key] = field.value;
+  });
+  const schemaString = JSON.stringify(formattedSchema, null, 2);
+  //  ---------------------------------------------------------
+
   return (
     <>
       <Breadcrumbs pageName={endpoint?.name} />
       <Header
         title={`${pageData?.title} : ${endpoint?.name}`}
       >{`${pageData?.description} : ${endpoint?.name}`}</Header>
+      <Separator />
       <SchemaTable schema={schema} />
+      <Separator />
+      <Craft.Article>
+        <h2>Posting Instructions</h2>
+        <p>
+          Use the following URL to post to{" "}
+          <span className="italic">{endpoint?.name}</span>:
+        </p>
+        <pre>{url}</pre>
+        <Separator />
+        <h3>Post via API POST Request</h3>
+        <p>
+          Send a POST request to the provided URL with the following body
+          payload structure:
+        </p>
+        <pre>{schemaString}</pre>
+        <p>Make sure to include the following API key as a header:</p>
+        <pre>{apiKey}</pre>
+        <p>A sample CURL request would look like the following:</p>
+        <pre>
+          {`curl -X POST ${url} \\`}
+          <br />
+          {`--header "Content-Type: application/json" \\`}
+          <br />
+          {`--header "Authorization: ${apiKey}" \\`}
+          <br />
+          {`--data '${schemaString}'`}
+        </pre>
+        <Separator />
+        <h3>Post via HTML Form</h3>
+      </Craft.Article>
     </>
   );
 }
--- components/groups/endpoints/schema-table.tsx
+++ components/groups/endpoints/schema-table.tsx
@@ -10,7 +10,7 @@ import {
 export default function SchemaTable({ schema }: { schema: GeneralSchema[] }) {
   return (
     <div className="max-w-lg">
-      <h2 className="my-2">Schema for your endpoint</h2>
+      <h2 className="my-2 text-xl md:text-3xl">Schema</h2>
       <Table>
         <TableHeader>
           <TableRow>
@@ -19,8 +19,8 @@ export default function SchemaTable({ schema }: { schema: GeneralSchema[] }) {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {schema.map((field) => (
-            <TableRow>
+          {schema.map((field, index) => (
+            <TableRow key={index}>
               <TableCell>{field?.key}</TableCell>
               <TableCell>{field?.value}</TableCell>
             </TableRow>
