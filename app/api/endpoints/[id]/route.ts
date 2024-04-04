be8b6a4127e36f0afc279875e9067bc22f64e069
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-04-04 13:38:41 -0600

    feat: add compatibility with HTML form posting

--- app/api/endpoints/[id]/route.ts
+++ app/api/endpoints/[id]/route.ts
@@ -40,6 +40,13 @@ export async function POST(
       .where(eq(endpoints.incrementId, parsedId));
     const endpoint = endpointResult[0];
 
+    if (!endpoint) {
+      return NextResponse.json(
+        { message: "Endpoint not found." },
+        { status: 404 }
+      );
+    }
+
     if (endpoint.token !== token) {
       return NextResponse.json(
         { message: "Unauthorized. Invalid token provided." },
@@ -122,11 +129,118 @@ export async function GET(
   { params }: { params: { id: string } }
 ) {
   const headersList = headers();
-  const referer = headersList.get("referer") as string;
-
+  const referer = headersList.get("referer");
+  // get URL search parameters
   const { searchParams } = new URL(request.url);
-  console.log(searchParams.toString());
-  console.log(params.id);
-  // return NextResponse.json({ message: "GET request" });
-  return NextResponse.redirect(new URL(referer));
+
+  // parse the ID from the URL and return a 400 if it's not a number
+  const parsedId = parseInt(params.id);
+  if (isNaN(parsedId)) {
+    return NextResponse.json(
+      { message: "Invalid ID provided." },
+      { status: 400 }
+    );
+  }
+
+  // get the endpoint from the database
+  const endpointResult = await db
+    .select()
+    .from(endpoints)
+    .where(eq(endpoints.incrementId, parsedId));
+  const endpoint = endpointResult[0];
+
+  // if the endpoint doesn't exist, return a 404
+  if (!endpoint) {
+    return NextResponse.json(
+      { message: "Endpoint not found." },
+      { status: 404 }
+    );
+  }
+
+  // get the data from the URL search parameters
+  const rawData: any = {};
+  for (const [key, value] of searchParams) {
+    rawData[key] = value;
+  }
+
+  // get the schema from the endpoint
+  const schema = endpoint?.schema as GeneralSchema[];
+
+  // convert the data to the correct types
+  const data = convertToCorrectTypes(rawData, schema);
+
+  // TODO: make this into its own function
+  const dynamicSchema = schema.reduce<z.ZodRawShape>((acc, { key, value }) => {
+    const validation = validations[value];
+    if (validation) {
+      acc[key as keyof SchemaToZodMap] = validation;
+    }
+    return acc;
+  }, {});
+
+  // create a Zod schema from the dynamic schema
+  const EndpointZodSchema = z.object(dynamicSchema);
+
+  // validate the data against the schema
+  Object.keys(dynamicSchema).forEach((key) => {
+    const validation = dynamicSchema[key];
+    console.log(`Field: ${key}, Validation: ${validation._def.typeName}`);
+  });
+
+  // if the data doesn't match the schema, return a 400
+  const parsedData = EndpointZodSchema.safeParse(data);
+  if (!parsedData.success) {
+    await db.insert(logs).values({
+      type: "error",
+      message: JSON.stringify(parsedData.error.format()),
+      createdAt: new Date(),
+      endpointId: endpoint.id,
+    });
+    revalidatePath("/logs");
+    return NextResponse.redirect(
+      new URL(endpoint?.failUrl || referer || "/fail")
+    );
+  }
+
+  // insert the lead into the database
+  const insertedLead = await db
+    .insert(leads)
+    .values({
+      data: parsedData.data,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      endpointId: endpoint.id,
+    })
+    .returning({ insertedId: leads.id });
+
+  await db.insert(logs).values({
+    type: "success",
+    message: { success: insertedLead[0].insertedId },
+    createdAt: new Date(),
+    endpointId: endpoint.id,
+  });
+  revalidatePath("/logs");
+
+  return NextResponse.redirect(new URL(endpoint?.successUrl));
+}
+
+// TODO: Move this to be its own file
+function convertToCorrectTypes(data: any, schema: GeneralSchema[]) {
+  const result: any = {};
+
+  schema.forEach(({ key, value }) => {
+    if (value === "boolean") {
+      // Convert "true" and "false" strings to boolean values
+      result[key] = data[key] === "true";
+    } else if (value === "number") {
+      // Convert string to number, ensuring NaN is handled appropriately
+      const num = Number(data[key]);
+      result[key] = isNaN(num) ? undefined : num;
+    } else {
+      // For all other types, assume string or no conversion needed
+      result[key] = data[key];
+    }
+  });
+
+  return result;
 }
