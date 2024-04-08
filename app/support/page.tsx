d41b911022b6f63ed33e5223d3d29c159165ee01
Bridger Tower <bridgertower@gmail.com>
2024-04-08 14:51:44 -0600

    add /settings and /support page

---
+++ app/support/page.tsx
@@ -0,0 +1,27 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth/auth";
+import { Breadcrumbs } from "@/components/parts/breadcrumbs";
+import { Header } from "@/components/parts/header";
+import PageWrapper from "@/components/page-wrapper";
+
+const pageData = {
+  name: "Support",
+  title: "Support",
+  description: "Get help with Router.so",
+};
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/login");
+
+  return (
+    <>
+      <Breadcrumbs pageName={pageData?.name} />
+      <PageWrapper>
+        <Header title={pageData?.title}>{pageData?.description}</Header>
+        <div className="grid gap-4 grid-rows-[500px,1fr]">Coming soon ...</div>
+      </PageWrapper>
+    </>
+  );
+}
