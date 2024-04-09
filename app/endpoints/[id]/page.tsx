ccc9bc10e9bebf0bef87b3f797a2f34f3ca81f01
Bridger Tower <bridgertower@gmail.com>
2024-04-09 09:24:37 -0600

    add copy button to all endpoints

--- app/endpoints/[id]/page.tsx
+++ app/endpoints/[id]/page.tsx
@@ -17,6 +17,7 @@ import { Home } from "lucide-react";
 import PageWrapper from "@/components/page-wrapper";
 import Image from "next/image";
 import Icon from "@/public/icon.svg";
+import CopyButton from "@/components/copy-button";
 
 const pageData = {
   title: "Endpoint",
@@ -42,34 +43,28 @@ export default async function Page({ params }: { params: { id: string } }) {
   const schemaString = JSON.stringify(formattedSchema, null, 2);
   //  ---------------------------------------------------------
 
+  const exampleCurl = `curl -X POST ${url} \\
+  --header "Content-Type: application/json" \\
+  --header "Authorization: Bearer ${endpoint?.token}" \\
+  --data '${schemaString}'`;
+
+  const exampleForm = `<form action="${url}" method="GET">
+    ${schema.map(
+      (field) =>
+        `<input type="${
+          field.value === "boolean"
+            ? "checkbox"
+            : field.value === "number"
+            ? "number"
+            : "text"
+        }" name="${field.key}" />`
+    )}
+    <button type="submit" value="Submit" />
+  </form>`;
+
   return (
     <>
-      <Breadcrumb className="h-[67.63px] bg-muted/50 rounded-lg border flex items-center justify-between p-6">
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/">
-              <Home size={20} />
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/endpoints">Endpoints</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage className="px-2 py-1 bg-accent rounded-sm">
-              {endpoint?.name}
-            </BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-        <Image
-          className="hover:animate-spin dark:invert"
-          src={Icon}
-          width={24}
-          height={24}
-          alt="Router.so Icon"
-        />
-      </Breadcrumb>
+      <Breadcrumbs endpoint={endpoint} />
       <PageWrapper>
         <Header
           title={`${pageData?.title}: ${"`"}${endpoint?.name}${"`"}`}
@@ -82,35 +77,42 @@ export default async function Page({ params }: { params: { id: string } }) {
             Use the following URL to post to the endpoint for:{" "}
             <span className="text-foreground">{endpoint?.name}</span>
           </p>
-          <pre>{url}</pre>
+          <pre className="relative">
+            {url}
+            <CopyButton text={url} />
+          </pre>
           <h3>Post via API POST Request</h3>
           <p>
             Send a POST request to the provided URL with the following body
             payload structure:
           </p>
-          <pre>{schemaString}</pre>
+          <pre className="relative">
+            {schemaString} <CopyButton text={schemaString} />
+          </pre>
           <p>Make sure to include the following API key as a header:</p>
-          <pre>{endpoint?.token}</pre>
+          <pre className="relative">
+            {/* @ts-ignore */}
+            {endpoint?.token} <CopyButton text={endpoint?.token} />
+          </pre>
           <p>A sample CURL request would look like the following:</p>
-          <pre>
-            {`curl -X POST ${url} \\`}
-            <br />
-            {`--header "Content-Type: application/json" \\`}
-            <br />
-            {`--header "Authorization: Bearer ${endpoint?.token}" \\`}
-            <br />
-            {`--data '${schemaString}'`}
+          <pre className="relative">
+            {exampleCurl} <CopyButton text={exampleCurl} />
           </pre>
           <Separator />
           {endpoint?.formEnabled && (
             <>
               <h3>Post via HTML Form</h3>
               <p>Add the following URL to the form action attribute:</p>
-              <pre>{url}</pre>
+              <pre className="relative">
+                {url} <CopyButton text={url} />
+              </pre>
               <p>Your HTML form element should look like this:</p>
-              <pre>{`<form action="${url}" method="GET">`}</pre>
-              <span className="text-sm italic text-muted-foreground">
-                *Make sure to use GET as the HTTP method.
+              <pre className="relative">
+                {`<form action="${url}" method="GET">`}{" "}
+                <CopyButton text={`<form action="${url}" method="GET">`} />
+              </pre>
+              <span className="text-sm text-muted-foreground">
+                Make sure to use GET as the HTTP method.
               </span>
               <p>
                 Add an input element for each of the elements in your schema.
@@ -118,43 +120,33 @@ export default async function Page({ params }: { params: { id: string } }) {
                 the HTML input.
               </p>
               <p>Your form should look something like this:</p>
-              <pre>
-                {`<form action="${url}" method="GET">`}
-                <br />
-                {schema.map((field) => (
-                  <span key={field.key}>
-                    {`  <input type="${
-                      field.value === "boolean"
-                        ? "checkbox"
-                        : field.value === "number"
-                        ? "number"
-                        : "text"
-                    }" name="${field.key}" />`}
-                    <br />
-                  </span>
-                ))}
-                {`  <button type="submit" value="Submit" />`}
-                <br />
-                {`</form>`}
+              <pre className="relative">
+                {exampleForm} <CopyButton text={exampleForm} />
               </pre>
               <Separator />
               <p className="text-sm text-muted-foreground">
-                *Ensure that your redirect URLs are set to your desired URLs
+                Ensure that your redirect URLs are set to your desired URLs
                 within the endpoint. Upon successful or failed submission, the
                 user will be redirected to these URLs. The best experience for
                 the user is to provide some sort of client-side validation on
                 your inputs so that the user cannot submit the form until all
                 inputs are valid. Router.so only does server-side validation.{" "}
-                <span className="text-red-500 italic">
+                <span className="text-red-500">
                   Please ensure you add validation on the client for the best
                   user experience.
                 </span>
               </p>
               <p>Success and fail URLs for this endpoint:</p>
-              <pre>{`Success URL: ${endpoint?.successUrl}`}</pre>
-              <pre>{`Fail URL: ${endpoint?.failUrl}`}</pre>
-              <p className="text-red-500 italic text-xs">
-                *Currently no authentication is required to post leads from a
+              <pre className="relative">
+                {`Success URL: ${endpoint?.successUrl}`}{" "}
+                <CopyButton text={`Success URL: ${endpoint?.successUrl}`} />
+              </pre>
+              <pre className="relative">
+                {`Fail URL: ${endpoint?.failUrl}`}{" "}
+                <CopyButton text={`Fail URL: ${endpoint?.failUrl}`} />
+              </pre>
+              <p className="text-red-400 text-xs">
+                Currently no authentication is required to post leads from a
                 form. However, we will be adding a client-side token that users
                 will be required to add to their forms as a hidden input field.
               </p>
@@ -163,5 +155,36 @@ export default async function Page({ params }: { params: { id: string } }) {
         </Craft.Article>
       </PageWrapper>
     </>
+  );
+}
+
+function Breadcrumbs({ endpoint }: { endpoint: any }) {
+  return (
+    <Breadcrumb className="h-[67.63px] bg-muted/50 rounded-lg border flex items-center justify-between p-6">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">
+            <Home size={20} />
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/endpoints">Endpoints</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage className="px-2 py-1 bg-accent rounded-sm">
+            {endpoint?.name}
+          </BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+      <Image
+        className="hover:animate-spin dark:invert"
+        src={Icon}
+        width={24}
+        height={24}
+        alt="Router.so Icon"
+      />
+    </Breadcrumb>
   );
 }
---
+++ components/copy-button.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import { Button } from "./ui/button";
+import { Copy } from "lucide-react";
+import { toast } from "sonner";
+
+type CopyButtonProps = {
+  text: string;
+};
+
+const CopyButton: React.FC<CopyButtonProps> = ({ text }) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    toast.success("Copied to clipboard");
+  };
+
+  return (
+    <Button
+      className="absolute right-2 top-2"
+      size="icon"
+      variant="secondary"
+      onClick={handleCopy}
+    >
+      <span className="sr-only">Copy</span>
+      <Copy size="16" />
+    </Button>
+  );
+};
+
+export default CopyButton;
