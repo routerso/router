a0e7a723a4546b2a91e3b33404aee6b09d8e8580
Bridger Tower <bridgertower@gmail.com>
2024-05-20 14:15:40 -0600

    Updated

--- components/groups/endpoints/create-form.tsx
+++ components/groups/endpoints/create-form.tsx
@@ -201,7 +201,6 @@ export default function CreateForm() {
           ))}
 
           <Button
-            variant={"outline"}
             onClick={() => {
               append({ key: "", value: "" });
             }}
