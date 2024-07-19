2317b000cdcc2fbdb526d9ff46e9db4edbbc8090
Bridger Tower <bridgertower@gmail.com>
2024-07-19 10:16:46 -0600

    fix overflow on some pages

--- main/components/parts/page-wrapper.tsx
+++ main/components/parts/page-wrapper.tsx
@@ -1,6 +1,6 @@
 export function PageWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <div className="p-6 border overflow-scroll no-scrollbar rounded-lg bg-muted/50 h-full">
+    <div className="p-6 border overflow-y-scroll no-scrollbar w-full sm:max-w-[calc(100vw-272px)] rounded-lg bg-muted/50 h-full">
       {children}
     </div>
   );
