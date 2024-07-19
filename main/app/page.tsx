51297cdef9b3b78dc5801e31993f1e25cb056686
Bridger Tower <bridgertower@gmail.com>
2024-07-19 09:53:34 -0600

    add links and update chart on dashboard

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -6,6 +6,9 @@ import { Header } from "@/components/parts/header";
 import { Chart } from "@/components/dashboard/chart";
 import { PageWrapper } from "@/components/parts/page-wrapper";
 
+import Link from "next/link";
+import { desc } from "drizzle-orm";
+
 const pageData = {
   name: "Dashboard",
   title: "Dashboard",
@@ -22,7 +25,48 @@ export default async function Page() {
       <PageWrapper>
         <Header title={pageData?.title}>{pageData?.description}</Header>
         <Chart />
+        <Links />
       </PageWrapper>
     </>
   );
 }
+
+const navLinks = [
+  {
+    name: "Endpoints",
+    description: "Create and Manage Router.so Endpoints",
+    href: "/endpoints",
+  },
+  {
+    name: "Leads",
+    description: "View Lead data and Form Submissions",
+    href: "/leads",
+  },
+  {
+    name: "Logs",
+    description: "Monitor your API usage and errors",
+    href: "/logs",
+  },
+  {
+    name: "Settings",
+    description: "Manage your account settings",
+    href: "/settings",
+  },
+];
+
+const Links = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4 my-4">
+      {navLinks.map((link) => (
+        <Link
+          className="bg-background p-4 rounded-lg border hover:bg-accent/75 transition-all"
+          key={link.href}
+          href={link.href}
+        >
+          {link.name}
+          <p className="text-sm text-gray-500">{link.description}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
