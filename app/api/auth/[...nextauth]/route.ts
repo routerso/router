f8aa4a533ec84b93a67c58930e5b8c6b2ae82780
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-08-07 15:27:03 -0700

    update: change auth routes to updated nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,2 @@
-import NextAuth from "next-auth/next";
-import { authOptions } from "@/lib/auth/auth";
-
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+import { handlers } from "@/lib/auth";
+export const { GET, POST } = handlers;
