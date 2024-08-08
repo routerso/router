94ce306b0383dbe1fd3b719b3c2bf1b6df8b9421
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-08-08 10:05:59 -0700

    update: change API key generation to 32 bytes from 64

--- app/api/endpoints/route.ts
+++ app/api/endpoints/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
 
     const data: EndpointPOSTPayload = await request.json();
-    const token = randomBytes(64).toString("hex");
+    const token = randomBytes(32).toString("hex");
 
     const createdEndpoint = await db
       .insert(endpoints)
