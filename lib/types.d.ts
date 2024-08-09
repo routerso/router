1bec24b4e47310e85c2a2ca82e7a394a2e26949f
Cameron Youngblood <cameron.youngblood@gmail.com>
2024-08-09 12:16:09 -0700

    docs: add docstrings to validation logic and types.d.ts

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,18 +1,35 @@
+/**
+ * Prop type for navigation
+ */
 type NavLinkProps = {
   href: string;
   children: React.ReactNode;
   icon: Icon;
 };
 
+/**
+ * Prop type for header
+ */
 type HeaderProps = {
   title: string;
   children?: React.ReactNode;
 };
 
+/**
+ * Represents a general schema.
+ *
+ * {Object.<string, ValidationType>} GeneralSchema
+ */
 type GeneralSchema = {
   [key: string]: ValidationType;
 };
 
+/**
+ * Validation types
+ *
+ * These can be extended
+ * * See instructions in @/lib/validation/index.ts
+ */
 type ValidationType =
   | "phone"
   | "email"
@@ -23,6 +40,9 @@ type ValidationType =
   | "url"
   | "zip_code";
 
+/**
+ * Payload type for POST requests to the /api/endpoints route
+ */
 type EndpointPOSTPayload = {
   userId: string;
   name: string;
@@ -35,6 +55,9 @@ type EndpointPOSTPayload = {
   webhook?: string;
 };
 
+/**
+ * Payload type for PUT requests to the /api/endpoints route
+ */
 type EndpointPUTPayload = {
   id: string;
   userId: string;
@@ -48,18 +71,31 @@ type EndpointPUTPayload = {
   webhook?: string;
 };
 
+/**
+ * Row type for the main dashboard data on /dashboard route
+ */
 type LeadAndErrorCountResults = {
   date: string;
   leads: number;
   errors: number;
 }[];
 
+/**
+ * Represents a mapping between keys of type `GeneralSchema["key"]`
+ * and the return types of the corresponding validation functions
+ * from the `validations` object.
+ */
 type SchemaToZodMap = {
   [P in GeneralSchema["key"]]: ReturnType<
     (typeof validations)[GeneralSchema["value"]]
   >;
 };
 
+/**
+ * Type for fetched logs
+ *
+ * includes attributes of the 'endpoint' db model
+ */
 type LogRow = {
   id: string;
   type: "success" | "error";
@@ -69,6 +105,28 @@ type LogRow = {
   createdAt: Date;
 };
 
+/**
+ * Type for fetched leads
+ *
+ * includes attributes of the 'endpoint' db model
+ */
+type LeadRow = {
+  id: string;
+  data: { [key: string]: any };
+  schema?: { key: string; value: ValidationType }[];
+  createdAt: Date;
+  updatedAt: Date;
+  endpointId: string;
+  endpoint?: string;
+};
+
+/**
+ * Represents a log message.
+ *
+ * This type can have two possible shapes:
+ * - If the log message is successfully parsed, it will have a shape defined by the `SafeParseReturnType` type.
+ * - If the log message is not successfully parsed, it will have a shape of `{ success: string }`.
+ */
 type LogMessage =
   | z.SafeParseReturnType<
       {
@@ -80,16 +138,12 @@ type LogMessage =
     >
   | { success: string };
 
-type LeadRow = {
-  id: string;
-  data: { [key: string]: any };
-  schema?: { key: string; value: ValidationType }[];
-  createdAt: Date;
-  updatedAt: Date;
-  endpointId: string;
-  endpoint?: string;
-};
-
+/**
+ * Represents a function that handles server actions.
+ *
+ * @param formData - The form data to be processed by the server action.
+ * @returns A promise that resolves to an object containing an error message, or undefined if there is no error.
+ */
 type ServerActionFunction = (
   formData: FormData
 ) => Promise<{ error: string } | undefined>;
