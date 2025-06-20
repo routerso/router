db59d0784d57b51f4774cd19ba1710b4038dfeed
Cameron Youngblood <cameron.youngblood@gmail.com>
2025-06-19 22:55:33 -0700

    add notice

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -19,6 +19,13 @@ export default async function LoginPage() {
           height={72}
         ></Image>
         <p className="text-center text-xl">Welcome to router.so</p>
+        <div className="rounded-md border border-yellow-500 border-dashed">
+          <p className="text-xs text-yellow-500 max-w-xs p-4">
+            On 6/19/25, our account with an upstream database provider was
+            deleted, losing past data. If you are on a paid plan, please reach
+            out to 9d8dev@gmail.com. We are very sorry for the inconvenience.
+          </p>
+        </div>
         <p className="text-center text-muted-foreground">
           Login to access your account.
         </p>
